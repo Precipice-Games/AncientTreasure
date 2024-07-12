5e7fd4d666011876191ecf6c754e7cdc18b986df
egoldfarb5 <emmaleigh.goldfarb1@marist.edu>
2024-07-12 15:00:36 -0400

    Levels updated

--- Scenes/level_2.gd
+++ Scenes/level_2.gd
@@ -19,27 +19,32 @@ func _process(delta):
 
 
 func _on_lava_body_entered(body):
-	if body.is_in_group("player"):
-		body.die()
+	if body.is_in_group("players"):
+		print("dies but not")
+		#body.die()
 		game_over()
 
 
 
 func _on_lever_1_body_entered(body):
 	$Lever1/Lever1.play("LeverTurnOn")
-	lever1Pulled = true
-	create_tween().tween_property($Lava4, "global_position", $Lava4.global_position + Vector2(980, -1450), 1)
+	if lever1Pulled == false:
+		create_tween().tween_property($Lava4, "global_position", $Lava4.global_position + Vector2(980, -1450), 1)
+		lever1Pulled = true
+	else:
+		lever1Pulled = true
+
 	
 
 func _on_button_1_body_entered(body):
 	button1 = true
-	$Button1/Button1.play("ButtonOn")
+	$MovingPlat/Button1/Button1.play("ButtonOn")
 	movePlat()
 
 
 func _on_button_2_body_entered(body):
 	button2 = true
-	$Button2/Button2.play("ButtonOn")
+	$MovingPlat/Button2/Button2.play("ButtonOn")
 	movePlat()
 
 func movePlat():
@@ -48,15 +53,13 @@ func movePlat():
 
 func _on_button_1_body_exited(body):
 	button1 = false
-	$button1.play("ButtonOff")
+	$MovingPlat/Button1/Button1.play("ButtonOff")
 	create_tween().tween_property($MovingPlat, "global_position", Vector2(2600, 60), 1)
 
 
 func _on_button_2_body_exited(body):
 	button2 = false
-	$button2.play("ButtonOff")
+	$MovingPlat/Button2/Button2.play("ButtonOff")
 	create_tween().tween_property($MovingPlat, "global_position", Vector2(2600, 60), 1)
 
 
-func _on_team_switch_body_entered(body):
-	pass
--- spring.gd
+++ spring.gd
@@ -1,6 +1,6 @@
 extends Area2D
 
-var bounce_force:int = 3000
+var bounce_force:int = 2600
 
 func _on_body_entered(body):
 	$SpringSpr.play("SpringUp")
